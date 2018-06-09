cfa36d2fe0e5ce68c8c63337ad7f2d056d6a2fd8
Piyush Pilaniya <piyushpilaniya98@gmail.com>
2018-06-09 13:34:04 +0530

    Create default.py

---
+++ Morse_only/omni/default.py
@@ -0,0 +1,21 @@
+#! /usr/bin/env morseexec
+
+
+from morse.builder import *
+from src.omni.builder.robots import Holonomicrobot, Target
+from src.omni.builder.actuators import Holonomicrobotactuator
+
+robot = Holonomicrobot()
+robot.add_default_interface('socket')
+
+pose = Pose()
+robot.append(pose)
+
+robot.add_default_interface('socket')
+
+target = Target()
+target.translate(x=-2)
+
+env = Environment('sandbox', fastmode = True)
+env.set_camera_location([-18.0, -6.7, 10.8])
+env.set_camera_rotation([1.09, 0, -1.14])
